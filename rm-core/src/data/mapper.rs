74f78236844934f40f95352ebb417b7beb95e7ce
Angry-Maid <mikita.savos.dev@gmail.com>
2025-01-19 13:34:05 +0100

    refactor: remove old files

--- rm-core/src/data/mapper.rs
+++
@@ -1,8 +0,0 @@
-use glam::Vec2;
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct GatherableMap {
-    pub outline_poly: Vec<Vec2>,
-    pub blockouts: Vec<[Vec2; 4]>,
-}
